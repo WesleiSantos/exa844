59fac7b85a268d6ad8b695a5348fa1a88b5673d6
WesleiSantos <weslei200912@gmail.com>
2023-03-09 10:34:49 -0300

    update

--- atividade3/DomParser.py
+++ atividade3/DomParser.py
@@ -5,19 +5,22 @@ ini = time.time()
 MapDocument = parse('map.osm')
 
 print("Starting DOM Parser...")
+nodes = []
 for c in MapDocument.getElementsByTagName("node"):	
 	tag = c.getElementsByTagName("tag")
-	amenity = ""
+	node = dict()
 	for t in tag:
-		if t.getAttribute("k") == "name" and amenity:
-			print("name: ", t.getAttribute("v"))
-			print("lat: ", c.getAttribute("lat"))
-			print("lon: ", c.getAttribute("lon"))
-			print("")
+		if t.getAttribute("k") == "name":
+			node["name"] = t.getAttribute("v")
 		if t.getAttribute("k") == "amenity":
-			amenity = t.getAttribute("v")
-			print("amenity: ", amenity)
+			node["amenity"] = t.getAttribute("v")
+	node["lat"] = c.getAttribute("lat")
+	node["lon"] = c.getAttribute("lon")
+	if node.get("amenity"):
+		nodes.append(node)
 
+for node in nodes:
+	print("name:", node.get("name"), "amenity:",node.get("amenity"), "lat:", node.get("lat"), "lon", node.get("lon"))
 fim = time.time()
 print("\nTime =", fim-ini)
 		
