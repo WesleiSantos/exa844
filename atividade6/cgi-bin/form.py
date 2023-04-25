542e613b717900651cefff5ddba82ba57eb54e4a
WesleiSantos <weslei200912@gmail.com>
2023-04-25 10:39:38 -0300

    update

--- atividade6/cgi-bin/form.py
+++ atividade6/cgi-bin/form.py
@@ -23,20 +23,33 @@ escrever_arquivo(f'{http_date}\n', "bd.txt")
 
 str = ler_arquivo("bd.txt")
 
+data = []
+
 print("Content-type: text/html; charset=utf-8")
 print("\n")
 print("<html><head><title>Post Example</title></head><body>")
+print("<form method='POST' action='http://10.65.215.64:8080/cgi-bin/blog.py'>")
+print("Autor: <br />")
+print("<input type='text' size='64' name='autor' value='--autor--' /><br /><br />")
+print("Mensagem: <br />")
+print("<textarea rows='3' cols='46' name='mensagem'>--mensagem--</textarea><br /><br />")
+print("<input type='submit' /> <input type='reset' />")
+print("</form>")
 with open('bd.txt', 'r') as f:
     while True:
         autor = f.readline()
         mensagem = f.readline()
         hora = f.readline()
+        val = dict(autor = autor,mensagem=mensagem,hora=hora)
+        data.append(val) 
         if not hora:
             break
-        print(autor + "<br>")
-        print(mensagem + "<br>")
-        print(hora + "<br>")
-        print("<hr>")
+data.reverse()
+for val in data:
+    print(val['autor'] + "<br>")
+    print(val['mensagem'] + "<br>")
+    print(val['hora'] + "<br>")
+    print("<hr>")
 print("<a href='../index.html'>Voltar</a>")
 print("</body></html>")
 
---
+++ atividade7/main.py
@@ -0,0 +1,40 @@
+import functions_framework
+import pymysql
+
+import json
+import time
+
+@functions_framework.http
+def hello_http(request):    
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    map = {}
+    if request_json and 'action' in request_json and request_json['action']=="put":
+        try:     
+            conn = pymysql.connect(   
+                host="35.193.183.149",
+                port = 3306,
+                user="root",
+                password="weslei200",
+                db="Blog",
+                cursorclass=pymysql.cursors.DictCursor
+            )
+
+            with conn:
+                with conn.cursor() as cursor:
+                    message = request_json['message']
+                    autor = request_json['autor']
+                    date = time.strftime('%Y-%m-%d %H:%M:%S')                    
+                    cursor.execute("insert into Messages (message,autor,date) values ('{}','{}','{}');".format(message, autor, date))
+                    conn.commit()
+                    map["message"] = "put executed successfully"     
+        except Exception as e:
+            map["message"] = 'Error: {}'.format(str(e))            
+    elif request_args and 'action' in request_args and request_json['action']=="get":
+        map["message"] = request_json['message']
+        map[]
+    else:
+        map["message"] = "action missing or with wrong value!"
+    
+    return json.dumps(map)
